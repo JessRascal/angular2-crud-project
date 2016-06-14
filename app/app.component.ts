1a240cd24f6c33dd5b7f62b1586556e240b7e1f3
Jess Rascal <Jess@Jessey.uk>
2016-06-14 23:29:53 +0100

    Navbar added. Also Bootstrap css configured with a custom theme.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from './navbar.component';
 
 @Component({
     selector: 'my-app',
     template: `
-    
-    `
+        <navbar></navbar>
+    `,
+    directives: [ NavBarComponent ]
 })
 
 export class AppComponent {
